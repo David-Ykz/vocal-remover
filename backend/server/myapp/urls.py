2a616cd9e286cc549100b54ede28b3687d1ee324
David-Ykz <kzdavid314@gmail.com>
2024-04-13 22:59:47 -0400

    refactored backend

--- backend/server/myapp/urls.py
+++ backend/server/myapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.testConnection),
     path('upload/', views.handleFileUpload),
 ]
--- backend/server/myapp/views.py
+++ backend/server/myapp/views.py
@@ -3,78 +3,42 @@ import base64
 from django.shortcuts import render
 import demucs.separate # requires "pip install soundfile"
 import os
+import requests
+
 
 # Create your views here.
 # myapp/views.py
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-def testConnection(request):
-    return JsonResponse({'message': 'Test'})
-
-@csrf_exempt
-def handleFile(request):
-    print('handle')
-    print(request.POST.keys)
-    print(request.POST['file'])
-    print(type(request.POST['file']))
-    return JsonResponse({'message': 'Recieved'})
-
-
-@csrf_exempt
-def oldhandleFileUpload(request):
-    if request.method == 'POST' and request.FILES.get('file'):
-        uploaded_file = request.FILES['file']
-        if uploaded_file.content_type != 'audio/mpeg':
-            return JsonResponse({'error': 'Invalid file format'}, status=400)
-
-        with open('uploadedFile.mp3', 'wb') as destination:
-            for chunk in uploaded_file.chunks():
-                destination.write(chunk)
-
-        demucs.separate.main(["--mp3", "--two-stems", "vocals", "-n", "mdx_extra", "uploadedFile.mp3"])
-
-        return JsonResponse({'message': 'File uploaded and saved successfully'})
+def readAudioToString(path):
+    if os.path.exists(path):
+        with open(path, 'rb') as file:
+            data = file.read()
+        return base64.b64encode(data).decode('utf-8')
     else:
-        return JsonResponse({'error': 'File upload failed'}, status=400)
+        print("Error: could not locate file at path: " + path)
+        return ""
+
+def saveAudio(file):
+    with open('audio.mp3', 'wb') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+def splitAudio(file):
+    saveAudio(file)
+    demucs.separate.main(['--mp3', '--two-stems', 'vocals', '-n', 'mdx_extra', 'audio.mp3'])
+    vocalString = readAudioToString('separated/mdx_extra/audio/vocals.mp3')
+    nonVocalString = readAudioToString('separated/mdx_extra/audio/no_vocals.mp3')
+    return vocalString, nonVocalString
+
 @csrf_exempt
 def handleFileUpload(request):
-    if request.method == 'POST' and request.FILES.get('file'):
-        uploaded_file = request.FILES['file']
-        if uploaded_file.content_type != 'audio/mpeg':
-            return JsonResponse({'error': 'Invalid file format'}, status=400)
+    audioFile = request.FILES['file']
+    vocals, nonVocals = splitAudio(audioFile)
 
-        with open('audio.mp3', 'wb') as destination:
-            for chunk in uploaded_file.chunks():
-                destination.write(chunk)
-
-        demucs.separate.main(['--mp3', '--two-stems', 'vocals', '-n', 'mdx_extra', 'audio.mp3'])
-        vocals_file_path = 'separated/mdx_extra/audio/vocals.mp3'
-        no_vocals_file_path = 'separated/mdx_extra/audio/no_vocals.mp3'
-
-        if os.path.exists(vocals_file_path) and os.path.exists(no_vocals_file_path):
-            with open(vocals_file_path, 'rb') as vocalsFile, open(no_vocals_file_path, 'rb') as noVocalsFile:
-                vocalsData = vocalsFile.read()
-                noVocalsData = noVocalsFile.read()
-
-            vocalsString = base64.b64encode(vocalsData).decode('utf-8')
-            noVocalsString = base64.b64encode(noVocalsData).decode('utf-8')
-
-            response_data = {
-                'vocals': vocalsString,
-                'no_vocals': noVocalsString
-            }
-
-            return JsonResponse(response_data)
-        else:
-            return JsonResponse({'error': 'Separated audio files not found'}, status=404)
-
-    else:
-        return JsonResponse({'error': 'File upload failed'}, status=400)
-
-
-def api_endpoint(request):
-    # Your logic to handle the API request
-    data = {'message': 'This is the API endpoint'}
-    return JsonResponse(data)
+    response_data = {
+        'vocals': vocals,
+        'no_vocals': nonVocals
+    }
+    return JsonResponse(response_data)
